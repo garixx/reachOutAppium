13e94fe052cad4c1cb66ef627ded25faa716948b
garixx <akirraint@gmail.com>
2018-12-03 22:24:20 +0200

    uiautomator sample

--- src/main/java/utils/DriverManager.java
+++ src/main/java/utils/DriverManager.java
@@ -1,13 +1,10 @@
 package utils;
 
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -21,11 +18,11 @@ public class DriverManager {
 
         caps.setPlatform(Platform.ANDROID);
         caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "7.1.1");
-//        caps.setCapability("appPackage","com.reachout");
-//        caps.setCapability("appActivity", "com.cleveroad.reachout.ui.activity.SplashActivity");
-        caps.setCapability(MobileCapabilityType.APP,new File("src\\main\\resources\\reachout.apk"));
+        caps.setCapability("appPackage","com.reachout");
+        caps.setCapability("appActivity", "com.cleveroad.reachout.ui.activity.SplashActivity");
+        //caps.setCapability(MobileCapabilityType.APP,new File("src\\main\\resources\\reachout.apk"));
         caps.setCapability("deviceName","emulator-5554");
-        //caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),caps);
         return driver;
