795078841eddd51c7d28811f9bd19eb87759be61
garixx <akirraint@gmail.com>
2018-12-16 19:40:03 +0200

    Update Etalon.java

--- src/main/java/utils/Etalon.java
+++ src/main/java/utils/Etalon.java
@@ -2,7 +2,7 @@ package utils;
 
 public enum Etalon {
     LOGIN_PAGE("src/main/resources/loginPageEtalon.png"),
-    SETTINGS_PAGE("src/main/resources/settingsPageEtalon.png"),
+    SETTINGS_PAGE("src/main/resources/settingsPageEtalon2.png"),
     SETTINGS_PAGE_SCROLLED("src/main/resources/settingsPageScrolled.png");
 
     String value;
